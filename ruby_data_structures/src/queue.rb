bd28f7e44063260f89ca06ac5a0ef58ce86f76f5
pieteradejong <pieter.a.dejong@gmail.com>
2013-10-21 23:41:45 -0700

    koans: passing almost all basic requirements. data structures: created queues and tests. passing all.

--- ruby_data_structures/src/queue.rb
+++ ruby_data_structures/src/queue.rb
@@ -1,4 +1,24 @@
 class Queue
   def initialize
+    @start = 0
+    @end = 0
+    @storage = []
   end
+
+  def length
+    @end - @start
+  end
+
+  def add item
+    @storage[self.length] = item
+    @end += 1
+  end
+
+  def remove
+    unless self.length <= 0
+      @start += 1
+      ret = @storage[@start - 1]
+    end
+  end
+
 end
